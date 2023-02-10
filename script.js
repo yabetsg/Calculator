86bf382af136cf5f01ec94953e1edb4eb9db3446
yabets <yabets.g2@gmail.com>
2023-02-09 19:58:10 -0500

    Implemented css to enhance calculator appearance

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const button = document.querySelectorAll('.numbers');
 const un = document.querySelector('.unique');
 const display = document.querySelector('#display');
+const display2 = document.querySelector('#display2');
 const equals = document.querySelector('#equals-btn');
 const operator = document.querySelectorAll('.operator');
+const clearBtn = document.querySelector('.clear-btn');
 display.innerText = '';
-const add = (num1,num2) =>{
+let displayValue =[];
+let first = '';
+let second = '';
+let op = '';
+let flag = true;
+
+let displayValue2 = 0;
+   
+const add = (   num1,num2) =>{
     return num1+num2;
 }
 const subtract = (num1,num2) =>{
-    return num1-num2;
+     return num1-num2;
 }
 
 const multiply = (num1,num2) =>{
@@ -20,10 +30,10 @@ const divide = (num1,num2) =>{
 }
 const operate = (operator,num1,num2) =>{
     switch(operator){
-        case "+": add(num1,num2); break;
-        case "-": subtract(num1,num2);break;
-        case "/": divide(num1,num2);break;
-        case "*": multiply(num1,num2);break;
+        case "+": return add(num1,num2); 
+        case "-": return subtract(num1,num2);
+        case "/": return divide(num1,num2);
+        case "*": return multiply(num1,num2);
     }
 }
 const clearDisplay = () => {
@@ -31,9 +41,15 @@ const clearDisplay = () => {
 }
 const updateDisplay = () =>{
     
-   let displayValue;
-   let displayValue2;
-   let symbol = "+";
+    
+        clearBtn.addEventListener('click', e => {
+            first = '';
+            second = '';
+            op = '';
+            display2.innerText = '';
+            clearDisplay();
+        });
+    
     buttonEvent();
     operatorEvent();
     equalsEvent();
@@ -47,22 +63,67 @@ const buttonEvent = () => {
     }
 }
 const operatorEvent = () => {
+    
     for (let j = 0; j < operator.length; j++) {
         operator[j].addEventListener('click', e => {
-          displayValue = display.innerText;
-         symbol =  e.target.value.toString();
          
+        symbol =  e.target.value.toString();
+        if(first == ''){
+             first=display.innerText;
+             op=symbol;
+             console.log(first);
+             console.log(op);
+             clearDisplay();
+        }else{
+            // if(first.length>0 && display.innerText != ''){
+            //     clearDisplay();
+            // }
+            //clearDisplay();
+            console.log(first); 
+            second =display.innerText;
             
+          
+              
+                 first = operate(op,parseInt(first),parseInt(second));
+              
+             op = symbol;
+           // op = symbol;
+            
+            display.innerText = first;
+            console.log(first);
+            console.log(op)
+            second = '';
+            clearDisplay();
+        }
+     
+           
+          
       });
     }
 }
+
+
 const equalsEvent = () => {
     equals.addEventListener('click', e => {
-        console.log("im being pressed");
-        console.log(displayValue);
-           // operate(symbol,displayValue,displayValue);
-          });
+        symbol = e.target.value.toString();
+        if (first == '') {
+            first = display.innerText;
+            op = symbol;
+        } else {
+            second = display.innerText;
+            first = operate(op, parseInt(first), parseInt(second));
+            display.innerText = first;
+            // display2.innerText = first;
+            second = '';
+            op = '';
+            first = '';
+    
+        }
+    });
 }
+
+
+
 updateDisplay();
 
 
