c121056682af7b20ead8976ecfbacb4d44c933fe
yabets <yabets.g2@gmail.com>
2023-02-10 23:15:22 -0500

    Fix common bugs Add features such as:  %, +/- Accept decimals

--- script.js
+++ script.js
@@ -1,130 +1,146 @@
-const button = document.querySelectorAll('.numbers');
-const un = document.querySelector('.unique');
-const display = document.querySelector('#display');
-const display2 = document.querySelector('#display2');
-const equals = document.querySelector('#equals-btn');
-const operator = document.querySelectorAll('.operator');
-const clearBtn = document.querySelector('.clear-btn');
-display.innerText = '';
-let displayValue =[];
-let first = '';
-let second = '';
-let op = '';
-let flag = true;
-
+const button = document.querySelectorAll(".numbers");
+const display = document.querySelector("#display");
+const equals = document.querySelector("#equals-btn");
+const operator = document.querySelectorAll(".operator");
+const clearBtn = document.querySelector(".clear-btn");
+display.innerText = "";
+let firstNum = "";
+let secondNum = "";
+let operation = "";
+let operationBtnCheck = false;
+let equalBtnCheck = false;
 let displayValue2 = 0;
-   
-const add = (   num1,num2) =>{
-    return num1+num2;
-}
-const subtract = (num1,num2) =>{
-     return num1-num2;
-}
 
-const multiply = (num1,num2) =>{
-    return num1*num2;
-}
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
 
-const divide = (num1,num2) =>{
-    return num1/num2;
-}
-const operate = (operator,num1,num2) =>{
-    switch(operator){
-        case "+": return add(num1,num2); 
-        case "-": return subtract(num1,num2);
-        case "/": return divide(num1,num2);
-        case "*": return multiply(num1,num2);
-    }
-}
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
+const operate = (operator, num1, num2) => {
+  switch (operator) {
+    case "+":
+      return Math.round(add(num1, num2) * 100) / 100;
+    case "-":
+      return Math.round(subtract(num1, num2) * 100) / 100;
+    case "/":
+      return Math.round(divide(num1, num2) * 100) / 100;
+    case "*":
+      return Math.round(multiply(num1, num2) * 100) / 100;
+  }
+};
 const clearDisplay = () => {
-    display.innerText = '';
-}
-const updateDisplay = () =>{
-    
-    
-        clearBtn.addEventListener('click', e => {
-            first = '';
-            second = '';
-            op = '';
-            display2.innerText = '';
-            clearDisplay();
-        });
-    
-    buttonEvent();
-    operatorEvent();
-    equalsEvent();
-    }
- 
-const buttonEvent = () => {
-    for (let i = 0; i < button.length; i++) {
-        button[i].addEventListener('click', e => {
-            display.innerText += e.target.value;
-        });
-    }
-}
-const operatorEvent = () => {
-    
-    for (let j = 0; j < operator.length; j++) {
-        operator[j].addEventListener('click', e => {
-         
-        symbol =  e.target.value.toString();
-        if(first == ''){
-             first=display.innerText;
-             op=symbol;
-             console.log(first);
-             console.log(op);
-             clearDisplay();
-        }else{
-            // if(first.length>0 && display.innerText != ''){
-            //     clearDisplay();
-            // }
-            //clearDisplay();
-            console.log(first); 
-            second =display.innerText;
-            
-          
-              
-                 first = operate(op,parseInt(first),parseInt(second));
-              
-             op = symbol;
-           // op = symbol;
-            
-            display.innerText = first;
-            console.log(first);
-            console.log(op)
-            second = '';
-            clearDisplay();
-        }
-     
-           
-          
-      });
-    }
-}
+  display.innerText = "";
+};
+const updateDisplay = () => {
+  clearBtn.addEventListener("click", (e) => {
+    firstNum = "";
+    secondNum = "";
+    operation = "";
+    display.innerText = "";
+    clearDisplay();
+  });
 
+  buttonEvent();
+  operatorEvent();
+  equalsEvent();
+};
+
+const buttonEvent = () => {
+  for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", (e) => {
+      if (button[i].value === "." && display.innerText.includes(".")) {
+        button[i].classList.remove("numbers");
+      } else if (
+        button[i].value === "-" &&
+        button[i].textContent == "-" &&
+        display.innerText.includes("-")
+      ) {
+        button[i].classList.remove("numbers");
+      } else if (
+        button[i].value === "-" &&
+        button[i].textContent == "+╱-" &&
+        display.innerText === ""
+      ) {
+        display.innerText += e.target.value;
+      } else if (button[i].value === "-" && button[i].textContent == "+╱-") {
+        display.innerText = parseFloat(display.innerText) * -1;
+      } else if (button[i].value === "%") {
+        display.innerText = parseFloat(display.innerText) / 100;
+      } else {
+        display.innerText += e.target.value;
+        operationBtnCheck = false;
+        equalBtnCheck = true;
+      }
+    });
+  }
+};
+
+const operatorEvent = () => {
+  for (let j = 0; j < operator.length; j++) {
+    operator[j].addEventListener("click", (e) => {
+      symbol = e.target.value.toString();
+      if (operationBtnCheck === false) {
+        if (firstNum == "") {
+          firstNum = display.innerText;
+          operation = symbol;
+          console.log(firstNum);
+          console.log(operation);
+          clearDisplay();
+        } else {
+          console.log(firstNum);
+          secondNum = display.innerText;
+          firstNum = operate(
+            operation,
+            parseFloat(firstNum),
+            parseFloat(secondNum)
+          );
+
+          operation = symbol;
+          display.innerText = firstNum;
+          secondNum = "";
+          clearDisplay();
+        }
+      }
+      operationBtnCheck = true;
+    });
+  }
+};
 
 const equalsEvent = () => {
-    equals.addEventListener('click', e => {
-        symbol = e.target.value.toString();
-        if (first == '') {
-            first = display.innerText;
-            op = symbol;
-        } else {
-            second = display.innerText;
-            first = operate(op, parseInt(first), parseInt(second));
-            display.innerText = first;
-            // display2.innerText = first;
-            second = '';
-            op = '';
-            first = '';
-    
-        }
-    });
-}
-
-
+  equals.addEventListener("click", (e) => {
+    symbol = e.target.value.toString();
+    if (firstNum == "") {
+      firstNum = display.innerText;
+      operation = symbol;
+    } else {
+      secondNum = display.innerText;
+      if (equalBtnCheck) {
+      
+        firstNum = operate(
+          operation,
+          parseFloat(firstNum),
+          parseFloat(secondNum)
+        );
+      }
+      if(firstNum.toString().includes("Infinity")){
+        firstNum = "You cant do that lol";
+      }
+      display.innerText = firstNum;
+      secondNum = "";
+      operation = "";
+      firstNum = "";
+      equalBtnCheck = false;
+    }
+  });
+};
 
 updateDisplay();
-
-
-
